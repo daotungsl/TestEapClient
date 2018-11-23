65029cc83326b57d9b4c3d13c1404ee72e870e99
daotungsl <daotungsl@gmail.com>
2018-11-23 11:31:49 +0700

    again

---
+++ TestEapClient/Data/TestEapClientContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestEapClient.Models
+{
+    public class TestEapClientContext : DbContext
+    {
+        public TestEapClientContext (DbContextOptions<TestEapClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestEapClient.Models.Employee> Employee { get; set; }
+    }
+}
